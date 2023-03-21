0d3d14e0980aff3b47521c1fc890084cb6dacdcb
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-21 15:16:36 +0700

    test commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+console.log("Check Dev")
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
   });
