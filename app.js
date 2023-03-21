1da9ffd4df9a785a4c22c19d35943aedfad09679
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-21 16:42:49 +0700

    added main route

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const app = express();
 const port = 3000;
+const mainRoute = require(`./src/routes/index`)
+app.use(cors({
+  origin: "*",
+  method:"*"
+}));
+app.use(helmet());
+app.use(xss());
+
+app.use('/', mainRoute);
+
+app.use("/", (req, res) => {
+  res.status(200).json({ status: "Connected to API PEW-WORLD successfully", statusCode: 200 });
+});
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-console.log("Check Dev")
-console.log("test")
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
-  });
+  console.log(`App running on port ${port}.`);
+});
