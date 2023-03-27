48dbece3e7872e3f92e3d0c8a9824fab20ca2007
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-27 09:31:07 +0700

    adjusted login info

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -11,6 +11,8 @@ const {
   createEmployer,
   createEmployee,
 } = require("../model/usersModel");
+const { getDetailEmployee } = require("../model/employeeModel");
+const { getDetailEmployer } = require("../model/employerModel");
 
 const UsersController = {
   registerUser: async (req, res, next) => {
@@ -141,7 +143,7 @@ const UsersController = {
       next(
         res
           .status(404)
-          .json({ status: 404, message: `Email / Password / Name missing` })
+          .json({ status: 404, message: `Email / Password missing` })
       );
       return;
     }
@@ -195,6 +197,52 @@ const UsersController = {
 
     return next(res.status(404).json({ status: 404, message: `Login failed` }));
   },
+
+  login: async (req,res)=>{
+    try {
+        if (!req.body.email || !req.body.password) {
+            res.status(404).json({status:404,message:`Email / Password missing`})
+        } 
+          let {rows:[users]} = await findUser(req.body.email)
+          if (!users) {
+            res.status(404).json({status:404,message:`Login failed, user with email ${req.body.email} not found`})
+          }
+          let verifyPassword = await argon2.verify(users.password, req.body.password);
+          if(!verifyPassword){
+            res.status(404).json({status:404,message:`Login failed, wrong password`})
+          }
+          let role = users.role;
+          
+          if(role === "employee"){
+            let data ={
+              id:users.id
+            }
+            let {rows:[employee]} = await getDetailEmployee(data)
+            let accessToken = generateAccessToken(users);
+            let refreshToken = generateRefreshToken(users);
+            console.log("accessToken", accessToken);
+            employee.accessToken = accessToken
+            employee.refreshToken = refreshToken
+            employee.role = role
+            return res.status(200).json({status:200,message:`Login success`,data:employee})
+          }
+          if(role === "employer"){
+            let data ={
+              id:users.id
+            }
+            let {rows:[employer]} = await getDetailEmployer(data)
+            let accessToken = generateAccessToken(users);
+            let refreshToken = generateRefreshToken(users);
+            employer.accessToken = accessToken
+            employer.refreshToken = refreshToken
+            employer.role = role
+            return res.status(200).json({status:200,message:`Login success`,data:employer})
+          }
+        
+        } catch (error) {
+          return res.status(404).json({ status: 404, message: error.message, data:error.data });
+        }
+},
 };
 
 module.exports = UsersController;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser,verifyUser,loginUser} = require("../controller/usersController")
+const {registerUser,verifyUser,login} = require("../controller/usersController")
 
 router.post('/register/:role', registerUser)
 router.post('/verify',verifyUser)
-router.post('/login',loginUser)
+router.post('/login',login)
 
 module.exports = router;
