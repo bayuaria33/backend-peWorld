b53d81e00b0739f8c75d6b05b12febd46d6daa6e
Bayu Prayogo <Bayuaria33@gmail.com>
2023-03-27 21:01:06 +0700

    added get exp, porto by user id

--- src/controller/portoController.js
+++ src/controller/portoController.js
@@ -11,6 +11,19 @@ const portoController = {
         }
         return res.status(200).json({msg: "success get my porto", data: response.rows});
     },
+    getUserPorto: async (req, res) => {
+        try {          
+            let id = req.params.id;
+            let response = await selectPortoByUserId(id)
+            // console.log(response);
+            if (response.rows.length === 0) {
+                return res.status(400).json({msg: `Failed getting porto of user ${id} , doesn't exist`});
+            }
+            return res.status(200).json({msg: `Success get porto of user ${id}`, data: response.rows});
+        } catch (error) {
+            return res.status(400).json({msg: error.message, data: error.data});
+        }
+    },
     getPortoById: async (req, res) => {
         try {          
             let id = req.params.id;
--- src/routes/portoRoute.js
+++ src/routes/portoRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getMyPorto, postPorto, updatePorto, delPorto, getPortoById} = require("../controller/portoController");
+const {getMyPorto, postPorto, updatePorto, delPorto, getPortoById, getUserPorto} = require("../controller/portoController");
 const {protect} = require('../middleware/auth')
 const upload = require('../middleware/uploadFilePorto')
 
 router.get('/myporto', protect, getMyPorto)
+router.get('/user/:id', getUserPorto)
 router.get('/:id', getPortoById)
 router.post('/', protect, upload.single('porto_photo'), postPorto)
 router.put('/:id', protect, upload.single('porto_photo'), updatePorto)
